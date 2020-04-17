38dba7dc6f277372132a2194d8deb330ee10d74f
mayconullrich <maycon123@gmail.com>
2020-04-17 19:10:43 -0300

    Mudanças a serem submetidas: 	modified:   src/repositories/TransactionsRepository.ts 	modified:   src/routes/transaction.routes.ts 	modified:   src/services/CreateTransactionService.ts
    Desafio OK

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -15,14 +21,75 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
+  // let income = 0;
+  // let outcome = 0;
+
+  // const newUsersList = usersList.filter((user, index, array) => user.credit > 500);
+
+  // const incomeFilter = this.transactions.filter(
+  //   transaction => transaction.type === 'income',
+  // );
+
+  // const incomeTotal = incomeFilter.map(a => a.)
+
+  // let soma = 0;
+  // const income = this.transactions.map(transaction => {
+  //   if (transaction.type === 'income') {
+  //     soma += transaction.value;
+  //   }
+  //   return soma;
+  // });
+
+  // while (this.transactions < this.transactions.length) {
+
+  // }
+
+  //   for( this.transactions in Array){
+  //     [codigo]
+  //  }
+
+  // const balance = {
+  //   income,
+  //   outcome: ,
+  //   total: income - outcome,
+  // };
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
